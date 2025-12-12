1dfcfd7b9b4512ff36ceff45f6eb5e6062492d63
KangMin9704 <wnrkdals123@gmail.com>
2025-12-12 16:33:55 +0900

    [Fix] LobbyUI
    [Feat] StageSelect
     - 버튼 클릭시 씬 전환 파트 누락되어 추가

---
+++ Assets/Scripts/UI/StageSelectUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectUI : MonoBehaviour
+{
+    public void OnClickStage1Button()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
