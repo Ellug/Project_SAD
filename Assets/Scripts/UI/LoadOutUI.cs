7b74c67df90a6fcc6b22b650f3e7f3b16a358df9
KangMin9704 <wnrkdals123@gmail.com>
2025-12-12 15:59:30 +0900

    [Feat] LobbyUI
    [Feat] LobbyUI
     - LobbyUI : LoadOut / StageSelect / KeyGuide 버튼 상호작용처리
    
     - LoadOutUI
       - 무기 버튼 클릭 시 다시 클릭 안되게  처리
       - 해당 무기의 Puck 출력되게 처리

---
+++ Assets/Scripts/UI/LoadOutUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadOutUI : MonoBehaviour
+{
+    [SerializeField] private Button _rifleButton;
+    [SerializeField] private Button _snipeButton;
+    [SerializeField] private Button _shotgunButton;
+    [SerializeField] private GameObject _riflePuck;
+    [SerializeField] private GameObject _snipePuck;
+    [SerializeField] private GameObject _shotgunPuck;
+
+
+    public void OnClickRifle()
+    {
+        SetAllInActive();
+        _riflePuck.SetActive(true);
+        _rifleButton.interactable = false;
+    }
+    public void OnClickSnipe()
+    {
+        SetAllInActive();
+        _snipePuck.SetActive(true);
+        _snipeButton.interactable = false;
+    }
+    public void OnClickShotgun()
+    {
+        SetAllInActive();
+        _shotgunPuck.SetActive(true);
+        _shotgunButton.interactable = false;
+    }
+
+    public void SetAllInActive()
+    {
+        _riflePuck.SetActive(false);
+        _rifleButton.interactable = true;
+        _snipePuck.SetActive(false);
+        _snipeButton.interactable = true;
+        _shotgunPuck.SetActive(false);
+        _shotgunButton.interactable = true;
+    }
+}
