a99db861efb1e9458671f16860c625aab6edcbda
done778 <57587831+done778@users.noreply.github.com>
2025-12-14 02:35:33 +0900

    [feat] Add Boss AOE pattern
    보스 장판 패턴 기본적인 구현

---
+++ Assets/Scripts/Patterns/PatternAreaOfEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PatternAreaOfEffect : PatternBase
+{
+    [SerializeField] private float _warningTime;
+    [SerializeField] private float _lifeTime;
+
+    private GameObject _warningImage;
+    private GameObject _attackAOE;
+    private GameObject _target;
+
+    void Awake()
+    {
+        base.Awake();
+        _warningImage = transform.GetChild(0).gameObject;
+        _attackAOE = transform.GetChild(1).gameObject;
+    }
+
+    void OnEnable()
+    {
+        _warningImage.SetActive(false);
+        _attackAOE.SetActive(false);
+    }
+
+    public override void Init(GameObject target)
+    {
+        _target = target;
+    }
+
+    protected override void PatternLogic()
+    {
+        StartCoroutine(AreaOfEffectLogic());
+    }
+
+    private IEnumerator AreaOfEffectLogic()
+    {
+        transform.position = _target.transform.position;
+        _warningImage.SetActive(true);
+        yield return new WaitForSeconds(_warningTime);
+        _warningImage.SetActive(false);
+        yield return new WaitForSeconds(0.8f);
+        _attackAOE.SetActive(true);
+        yield return new WaitForSeconds(_lifeTime);
+        _attackAOE.SetActive(false);
+    }
+}
