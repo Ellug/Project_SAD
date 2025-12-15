1ff56edd3e4efaf8e256f4b1374a3c409e8af095
done778 <57587831+done778@users.noreply.github.com>
2025-12-15 15:26:06 +0900

    [comment] 프리팹, 스크립트 폴더링
    패턴 베이스를 상속받는 패턴 클래스와 장애물 컨트롤러를 구분했습니다. 같이 있으니까 헷갈려요.

--- Assets/Scripts/Patterns/LaserPattern.cs
+++ Assets/Scripts/Patterns/LaserPattern.cs
@@ -5,7 +5,7 @@ public class LaserPattern : PatternBase
     [Tooltip("오브젝트 태그")][SerializeField] GameObject[] _objectTag;
     public LaserObject[] _laserObject;
 
-    private void Awake()
+    protected override void Awake()
     {
         base.Awake();
         _objectTag = GameObject.FindGameObjectsWithTag("LaserObject");
@@ -13,21 +13,19 @@ public class LaserPattern : PatternBase
 
     public void ActivateObjects()
     {
-        _laserObject = new LaserObject[_objectTag.Length];
-        for (int i = 0; i < _objectTag.Length; i++)
-            _laserObject[i] = _objectTag[i].GetComponent<LaserObject>();
-
         for (int i = 0; i < _laserObject.Length; i++)
             _laserObject[i].ActivateObject();
     }
 
     protected override void PatternLogic()
     {
-
+        ActivateObjects();
     }
 
     public override void Init(GameObject target)
     {
-
+        _laserObject = new LaserObject[_objectTag.Length];
+        for (int i = 0; i < _objectTag.Length; i++)
+            _laserObject[i] = _objectTag[i].GetComponent<LaserObject>();
     }
 }
