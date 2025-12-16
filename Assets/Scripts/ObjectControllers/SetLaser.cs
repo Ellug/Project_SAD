3cf8a426c30b72b0d145ca383106e4141893f73d
ThePassingNoah <ansckdals319@gmail.com>
2025-12-16 15:28:21 +0900

    [Feat] Laser Shaer, Laser Particle
    레이저 패턴에 사용되는 레이저의 셰이더와 파티클을 추가합니다.

--- Assets/Scripts/ObjectControllers/SetLaser.cs
+++ Assets/Scripts/ObjectControllers/SetLaser.cs
@@ -2,22 +2,45 @@
 
 public class SetLaser : MonoBehaviour
 {
-    public LineRenderer lineRenderer;
-    public Transform firePoint; 
-    public float maxLaserDistance = 50f;
+    [Tooltip("레이저 라인렌더러")] public LineRenderer lineRenderer;
+    [Tooltip("레이저 시작지점")]  public Transform firePoint;
+    [Tooltip("레이저 최대 길이")] public float maxLaserDistance = 50f;
+    [Tooltip("레이저 히트 지점")] public GameObject laserHitObject;
+    [Tooltip("레이저 히트 이펙트 이격 거리")] public float hitParticleOffset = 0.1f;
 
     void Update()
     {
         RaycastHit hit;
+
         if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, maxLaserDistance))
         {
             lineRenderer.SetPosition(0, firePoint.position);
             lineRenderer.SetPosition(1, hit.point);
+
+            if (laserHitObject != null)
+            {
+                Vector3 offsetPosition = hit.point + (-firePoint.forward * hitParticleOffset);
+
+                laserHitObject.transform.position = offsetPosition;
+
+                Vector3 lookDirection = firePoint.position - laserHitObject.transform.position;
+                laserHitObject.transform.rotation = Quaternion.LookRotation(lookDirection);
+
+                if (!laserHitObject.activeSelf)
+                {
+                    laserHitObject.SetActive(true);
+                }
+            }
         }
         else
         {
             lineRenderer.SetPosition(0, firePoint.position);
             lineRenderer.SetPosition(1, firePoint.position + firePoint.forward * maxLaserDistance);
+
+            if (laserHitObject != null && laserHitObject.activeSelf)
+            {
+                laserHitObject.SetActive(false);
+            }
         }
     }
 }
