79308a57a772c35e05747790a9372ccff25eb84f
Ellug <auron975@nate.com>
2025-12-16 16:06:11 +0900

    [Feat] Object Pooling [Generic, Dic, Queue]
    - Add
      - PoolManager
      - PoolMember
      - IPoolable
    
    - Fix
      - Bullets
      - WeaponController, PatternShootin : apply pool to fire

--- Assets/Scripts/Bullets/BulletBase.cs
+++ Assets/Scripts/Bullets/BulletBase.cs
@@ -1,7 +1,72 @@
 ﻿using UnityEngine;
 
-public abstract class BulletBase : MonoBehaviour
+public abstract class BulletBase : MonoBehaviour, IPoolable
 {
-     // 탄환의 다양성을 고려하여 탄환 베이스를 만듦.
-     // 플레이어, 보스 탄환 외에도 탄환의 확장성을 고려하고 있다면 해당 클래스를 수정.
+     [SerializeField] protected float _speed = 20f;
+     [SerializeField] protected float _maxDistance = 50f; // 0 하면 사거리 무한
+     [SerializeField] protected float _dmg = 10f;
+
+     private Vector3 _spawnPos;
+     private PoolMember _poolMember;
+
+     protected virtual void Awake()
+     {
+          _poolMember = GetComponent<PoolMember>();
+     }
+
+     public virtual void Init(float dmg, float speed, float maxDistance)
+     {
+          _dmg = dmg;
+          _speed = speed;
+          _maxDistance = maxDistance;
+
+          _spawnPos = transform.position;
+     }
+
+     protected virtual void Update()
+     {
+          MoveForward();
+          CheckDistance();
+     }
+
+     // 정면 방향으로 전진
+     protected virtual void MoveForward()
+     {
+          transform.Translate(Vector3.forward * (_speed * Time.deltaTime), Space.Self);
+     }
+
+     // 최대 사거리 체크 후 디스폰
+     private void CheckDistance()
+     {
+          if (_maxDistance > 0f)
+          {
+               float sqr = (transform.position - _spawnPos).sqrMagnitude;
+               float maxSqr = _maxDistance * _maxDistance;
+
+               if (sqr >= maxSqr)
+               {
+                    Despawn();
+                    return;
+               }
+          }
+     }
+
+     protected void Despawn()
+     {
+          if (_poolMember == null)
+               _poolMember = GetComponent<PoolMember>();
+
+          if (_poolMember != null)
+               _poolMember.Despawn();
+          else
+               Destroy(gameObject);
+     }
+
+     // 특수탄환의 스폰, 디스폰시 추가 로직 세팅 용
+     public virtual void OnSpawned()
+     {
+          _spawnPos = transform.position;
+     }
+
+     public virtual void OnDespawned() {}
 }
