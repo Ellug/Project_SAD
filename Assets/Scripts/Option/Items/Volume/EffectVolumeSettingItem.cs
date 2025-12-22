ce73026d91eee8e1a936e5bc335d370febbd8ce4
KangMin9704 <wnrkdals123@gmail.com>
2025-12-22 15:28:59 +0900

    [Feat] Title/Setting UI
    타이틀 및 설정 UI 출력/구현 처리
     - Audio 처리
     - 해상도/창모드 처리

---
+++ Assets/Scripts/Option/Items/Volume/EffectVolumeSettingItem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EffectVolumeSettingItem : SettingItem
+{
+    [SerializeField] private Slider slider;
+
+    private void Awake()
+    {
+        slider.onValueChanged.AddListener(OnChanged);
+    }
+    private void OnEnable()
+    {
+        Refresh();
+    }
+
+    private void OnChanged(float value)
+    {
+        SettingManager.Instance.SetEffectVolume(value);
+    }
+
+    public override void Refresh()
+    {
+        slider.SetValueWithoutNotify(
+            SettingManager.Instance.Data.effectVolume
+            );
+    }
+}
