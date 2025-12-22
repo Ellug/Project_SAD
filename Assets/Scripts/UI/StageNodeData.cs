77564e57eab0e7fab608776c6ca51ebc5745e6a0
done778 <57587831+done778@users.noreply.github.com>
2025-12-22 12:06:00 +0900

    [feat] Add Stage Select UI in Lobby Scene
    로비 씬의 스테이지 선택 기능 구현

---
+++ Assets/Scripts/UI/StageNodeData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageNodeData", menuName = "ScriptableObject/StageNode")]
+public class StageNodeData : ScriptableObject
+{
+    [SerializeField] private string _stageNumber;
+    [SerializeField] private string _areaInfo;
+    [SerializeField] private string _bossInfo;
+
+    public string StageNumber => _stageNumber;
+    public string AreaInfo => _areaInfo;
+    public string BossInfo => _bossInfo;
+}
