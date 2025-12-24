611a54660925fdedcbb04f3f26c5b89026083d24
done778 <57587831+done778@users.noreply.github.com>
2025-12-24 13:31:31 +0900

    [feat] Apply Animation Settings Panel Open/Close
    설정 패널 열고 닫힐 때 애니메이션 DOTween으로 구현

--- Assets/Scripts/Option/Button/Option/SettingButton.cs
+++ Assets/Scripts/Option/Button/Option/SettingButton.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 public class SettingButton : MonoBehaviour
 {
+    [SerializeField] private GameObject _uiAnimation;
     [SerializeField] private GameObject _settingMenuPanel;
     [SerializeField] private GameObject _soundSettingPanel;
     [SerializeField] private GameObject _graphicSettingPanel;
 
     private void OnEnable()
     {
-        OnClickReturn();
+        PlayOpenAnimation();
     }
 
     //SettingMenuPanel
@@ -33,13 +35,30 @@ public class SettingButton : MonoBehaviour
 
     public void OnClickExit()
     {
-        UIManager.Instance.CloseTopUI();
+        PlayCloseAnimation();
     }
 
-    //private void CloseAll()
-    //{
-    //    _settingMenuPanel.SetActive(false);
-    //    _soundSettingPanel.SetActive(false);
-    //    _graphicSettingPanel.SetActive(false);
-    //}
+
+    private void PlayOpenAnimation()
+    {
+        Sequence seq = DOTween.Sequence();
+        _soundSettingPanel.SetActive(false);
+        _graphicSettingPanel.SetActive(false);
+        _uiAnimation.transform.localScale = new Vector3(0f, 1f, 1f);
+        _settingMenuPanel.transform.localScale = new Vector3(1f, 0f, 1f);
+        seq.Append(_uiAnimation.transform.DOScaleX(1f, 0.3f).SetEase(Ease.InCubic));
+        seq.Append(_uiAnimation.transform.DOScale(0f, 0f));
+        seq.Append(_settingMenuPanel.transform.DOScaleY(1f, 0.1f));
+    }
+
+    private void PlayCloseAnimation()
+    {
+        Sequence seq = DOTween.Sequence();
+        seq.Append(_settingMenuPanel.transform.DOScaleY(0f, 0.1f));
+        seq.Append(_uiAnimation.transform.DOScale(1f, 0f));
+        seq.Append(_uiAnimation.transform.DOScaleX(0f, 0.4f).SetEase(Ease.OutQuart));
+        seq.OnComplete(() => { 
+            UIManager.Instance.CloseTopUI(); 
+        });
+    }
 }
