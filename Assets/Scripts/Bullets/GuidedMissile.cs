139b84b2ee6fc49833b76ab82f3707ecba80567e
ThePassingNoah <ansckdals319@gmail.com>
2025-12-26 15:01:34 +0900

    [Fix] Boss Pattern
    ## 조정내용
    전달 받은 에셋에 맞도록 보스 패턴 제조정
    
    ## 세부 내용
    GuideMissliePattern 재조정 완료
    BossCannonPattern 재조정 완료
    PrecisionStrikePattern 재조정 완료
    LaserBombimgPattern 조정 시작

--- Assets/Scripts/Bullets/GuidedMissile.cs
+++ Assets/Scripts/Bullets/GuidedMissile.cs
@@ -1,81 +1,141 @@
 ﻿using UnityEngine;
-using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public class GuidedMissile : MonoBehaviour, IPoolable
 {
     private PoolMember _poolMember;
-    [Tooltip("폭발 파티클")][SerializeField] ParticleSystem _ExplosionParticle;
-    [Tooltip("미사일 판정 범위")][SerializeField] float _MisssileRange;
-    [Tooltip("미사일 지속 시간")][SerializeField] float _MisssileLifeTime;
-    [Tooltip("미사일 이동 속도")][SerializeField] float _MisssileSpeed = 15f;
-    [Tooltip("미사일 회전 속도")][SerializeField] float _MisssileRotationSpeed = 3f;
-    [Tooltip("데미지")][SerializeField] float _Dmg;
-    [Tooltip("플레이어")][SerializeField] private GameObject Player;
-    [Tooltip("충돌 레이어")][SerializeField] private LayerMask Layer;
-    private ParticleSystem _Explosion;
 
-    void Awake() 
+    [Header("VFX 프리팹")]
+    [Tooltip("총구 화염 효과 프리팹")][SerializeField] private GameObject _MuzzlePrefab;
+    [Tooltip("폭발 파티클 프리팹")][SerializeField] private GameObject _ExplosionPrefab;
+    [Tooltip("미사일에 붙어있는 잔상(Trail) 리스트")] public List<GameObject> _Trails;
+
+    [Header("미사일 설정")]
+    [Tooltip("미사일 지속 시간")][SerializeField] private float _MissileLifeTime = 5f;
+    [Tooltip("미사일 이동 속도")][SerializeField] private float _MissileSpeed = 15f;
+    [Tooltip("미사일 회전 속도")][SerializeField] private float _MissileRotationSpeed = 3f;
+    [Tooltip("데미지")][SerializeField] private float _Dmg;
+
+    [Header("플레이어")]
+    [Tooltip("플레이어")][SerializeField] private GameObject Player;
+
+    private bool _isDespawning = false;
+
+    private void Awake() 
     {
-        _poolMember = GetComponent<PoolMember>();
-        if (_MisssileLifeTime != 0) 
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    public void OnSpawned()
+    {
+        _isDespawning = false;
+
+        SpawnMuzzleFlash();
+
+        if (_MissileLifeTime > 0)
         {
-            Invoke("DespawnMissile", _MisssileLifeTime);
+            Invoke(nameof(DespawnMissile), _MissileLifeTime);
         }
     }
 
-    void Update() 
+    void Update()
     {
-        if (Player == null) return;
+        if (Player == null || _isDespawning) return;
 
         Vector3 direction = (Player.transform.position - transform.position).normalized;
-
         Quaternion lookRotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * _MisssileRotationSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * _MissileRotationSpeed);
 
-        transform.Translate(Vector3.forward * _MisssileSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * _MissileSpeed * Time.deltaTime);
+    }
 
-        bool isHit = Physics.CheckSphere(transform.position, _MisssileRange, Player.layer);
-        if (isHit)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_isDespawning) return;
+
+        if (other.CompareTag("Obstacle") || other.CompareTag("Player"))
         {
-            Player.TryGetComponent<PlayerModel>(out var player);
-            player.TakeDamage(_Dmg);
-            DespawnMissile();
-        }
+            if (other.CompareTag("Player") && other.TryGetComponent<PlayerModel>(out var player))
+            {
+                player.TakeDamage(_Dmg);
+            }
 
-        bool objectHit = Physics.CheckSphere(transform.position, _MisssileRange, Layer);
-        if (objectHit)
-        {
             DespawnMissile();
         }
     }
 
-    private void DespawnMissile() 
+    private void DespawnMissile()
     {
-        _Explosion = Instantiate(_ExplosionParticle, transform.position, transform.rotation);
-        var main = _Explosion.main;
-        main.stopAction = ParticleSystemStopAction.Destroy;
-        _Explosion.Play();
+        if (_isDespawning) return;
+        _isDespawning = true;
+
+        CancelInvoke(nameof(DespawnMissile));
+
+        HandleTrails();
+
+        SpawnExplosion();
+
         PoolManager.Instance.Despawn(gameObject);
     }
 
-    private void OnDrawGizmos()
+    private void SpawnMuzzleFlash()
     {
-        if (transform.position == null) return;
+        if (_MuzzlePrefab == null) return;
 
-        // 실제 로직과 동일한 체크 수행 (디버그용)
-        bool isHit = Physics.CheckSphere(transform.position, _MisssileRange, Layer);
-
-        // 감지되면 초록색, 아니면 빨간색
-        Gizmos.color = isHit ? Color.green : Color.red;
-
-        // 원 그리기
-        Gizmos.DrawWireSphere(transform.position, _MisssileRange);
-    }
-    public void OnSpawned()
-    {
+        GameObject muzzleVFX = Instantiate(_MuzzlePrefab, transform.position, transform.rotation);
+        float duration = GetParticleDuration(muzzleVFX);
+        Destroy(muzzleVFX, duration);
     }
 
-    public void OnDespawned()
+    private void SpawnExplosion()
     {
+        if (_ExplosionPrefab == null) return;
+
+        GameObject explosionVFX = Instantiate(_ExplosionPrefab, transform.position, transform.rotation);
+
+        var ps = explosionVFX.GetComponent<ParticleSystem>();
+        if (ps != null)
+        {
+            var main = ps.main;
+            main.stopAction = ParticleSystemStopAction.Destroy;
+            ps.Play();
+        }
+        else
+        {
+            Destroy(explosionVFX, GetParticleDuration(explosionVFX));
+        }
+    }
+
+    private void HandleTrails()
+    {
+        foreach (var trail in _Trails)
+        {
+            if (trail == null) continue;
+
+            trail.transform.parent = null;
+            var ps = trail.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                ps.Stop();
+                Destroy(trail, ps.main.duration + ps.main.startLifetime.constantMax);
+            }
+        }
+    }
+
+    private float GetParticleDuration(GameObject vfx)
+    {
+        var ps = vfx.GetComponent<ParticleSystem>();
+        if (ps != null) return ps.main.duration;
+
+        if (vfx.transform.childCount > 0)
+        {
+            var childPS = vfx.transform.GetChild(0).GetComponent<ParticleSystem>();
+            if (childPS != null) return childPS.main.duration;
+        }
+        return 2f;
+    }
+
+    public void OnDespawned() 
+    { 
     }
 }
--- Assets/Scripts/Patterns/LaserBombingPattern.cs
+++ Assets/Scripts/Patterns/LaserBombingPattern.cs
@@ -1,5 +1,130 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class LaserBombingPattern : PrecisionStrikePattern
+public class LaserBombingPattern : PatternBase
 {
+    [Header("경고 장판")]
+    [Tooltip("추적용 경고 파티클")][SerializeField] private ParticleSystem _WarnningArea;
+    [Tooltip("정지용 경고 파티클")][SerializeField] private ParticleSystem _StaticWarnningArea;
+    [Tooltip("경고 파티클 추적 시간")][SerializeField] private float _WarnningTime;
+    [Tooltip("경고 장판 정지 후 대기 시간")][SerializeField] private float _WarnningDTime;
+
+    [Header("폭발 설정")]
+    [Tooltip("첫 번째 폭발 파티클")][SerializeField] private ParticleSystem _FirstExplosionParticle;
+    [Tooltip("두 번째 폭발 파티클")][SerializeField] private ParticleSystem _SecondExplosionParticle;
+    [Tooltip("생성 위치 예측 오프셋")][SerializeField] private float _ChaseOffset;
+    [Tooltip("폭발 사이의 간격 시간")][SerializeField] private float _ExpolsionTime;
+    [Tooltip("폭발 판정 범위")][SerializeField] private float _ExplosionRange;
+
+    [Header("데미지")]
+    [Tooltip("데미지")][SerializeField] private float _Dmg;
+
+    private PredictiveAim _predictiveAim;
+    private GameObject Player;
+    private ParticleSystem _Warnning;
+    private ParticleSystem _StaticWarnning;
+    private bool chase = false;
+
+    private Vector3 _debugFirstPos;
+    private Vector3 _debugSecondPos;
+    private bool _showDebug = false;
+
+    void Update()
+    {
+        if (chase && _Warnning != null && _predictiveAim != null)
+        {
+            _Warnning.transform.position = _predictiveAim.PredictiveAimCalc(_ChaseOffset);
+        }
+    }
+
+    public void WarnningEffect()
+    {
+        if (_WarnningArea == null) return;
+
+        _showDebug = false;
+        _Warnning = Instantiate(_WarnningArea);
+        _Warnning.transform.position = _predictiveAim.PredictiveAimCalc(_ChaseOffset);
+        chase = true;
+
+        _Warnning.Play();
+        StartCoroutine(ChaseRoutine());
+    }
+
+    private IEnumerator ChaseRoutine()
+    {
+        yield return new WaitForSeconds(_WarnningTime);
+
+        chase = false;
+
+        if (_Warnning != null)
+        {
+            Vector3 stopPos = _Warnning.transform.position;
+            Destroy(_Warnning.gameObject);
+
+            if (_StaticWarnningArea != null)
+            {
+                _StaticWarnning = Instantiate(_StaticWarnningArea, stopPos, Quaternion.identity);
+                _StaticWarnning.Play();
+            }
+        }
+
+        yield return new WaitForSeconds(_WarnningDTime);
+
+        if (_StaticWarnning != null) Destroy(_StaticWarnning.gameObject);
+
+        StartCoroutine(ExplosionSequence());
+    }
+
+    private IEnumerator ExplosionSequence()
+    {
+        Vector3 explosionPos = (_StaticWarnning != null) ? _StaticWarnning.transform.position : transform.position;
+        _debugFirstPos = explosionPos;
+        _showDebug = true;
+
+        SpawnExplosion(_FirstExplosionParticle, explosionPos);
+        CheckDamage(explosionPos);
+
+        yield return new WaitForSeconds(_ExpolsionTime);
+
+        _debugSecondPos = explosionPos;
+        SpawnExplosion(_SecondExplosionParticle, explosionPos);
+        CheckDamage(explosionPos);
+    }
+
+    private void SpawnExplosion(ParticleSystem particlePrefab, Vector3 position)
+    {
+        if (particlePrefab == null) return;
+
+        ParticleSystem exp = Instantiate(particlePrefab, position, Quaternion.identity);
+        var main = exp.main;
+        main.stopAction = ParticleSystemStopAction.Destroy;
+        exp.Play();
+    }
+
+    private void CheckDamage(Vector3 position)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, _ExplosionRange);
+        foreach (var hit in colliders)
+        {
+            if (hit.gameObject == Player)
+            {
+                if (Player.TryGetComponent<PlayerModel>(out var player))
+                {
+                    player.TakeDamage(_Dmg);
+                }
+                break;
+            }
+        }
+    }
+
+    protected override void PatternLogic()
+    {
+        WarnningEffect();
+    }
+
+    public override void Init(GameObject target)
+    {
+        Player = target;
+        _predictiveAim = GameObject.FindAnyObjectByType<PredictiveAim>();
+    }
 }
