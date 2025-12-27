9fa3392059f98263ea636aa5a1fa59e155c0b1ee
Ellug <auron975@nate.com>
2025-12-27 15:09:00 +0900

    [Feat] 5 stage Perks Setting
    -5 shotgun heal need to dev

--- Assets/Scripts/Perks/PerksCalculator.cs
+++ Assets/Scripts/Perks/PerksCalculator.cs
@@ -20,7 +20,7 @@ public static class PerkCalculator
                 case StatId.Player_DodgeCoolTime: ClacFloat(ref s.DodgeCoolTime, m); break;
                 case StatId.Player_SpecialCoolTime: ClacFloat(ref s.SpecialCoolTime, m); break;
                 case StatId.Player_AttackSlowRate: ClacFloat(ref s.AttackSlowRate, m); break;
-                case StatId.Player_AttackMinSpeed: ClacFloat(ref s.AttackMinSpeed, m); break;                
+                case StatId.Player_AttackMinSpeed: ClacFloat(ref s.AttackMinSpeed, m); break;
             }
         }
     }
